487ccefd57d88480b90d9f86406caa125754850b
Rexagon <reide740@gmail.com>
2018-10-20 16:16:13 +0300

    Fix (added files for previous commit)

--- cancert/main.cpp
+++ cancert/main.cpp
@@ -2,18 +2,59 @@
 
 using namespace cv;
 
-int main(int argc, char* argv[])
+Mat origin;
+
+struct ThresholdState
 {
-	Mat originalMat = imread(argv[1], IMREAD_GRAYSCALE); //input image
-	imshow("Original", originalMat);
+	int adaptiveThreshold = 100;
+	int blockSize = 43;
+	int C = 150;
+} thresholdState;
 
-	Ptr<CLAHE> clahe = createCLAHE();
-	clahe->setClipLimit(0.5);
+double getMean(Mat channel)
+{
+	int histSize = 256;    // bin size
+	float range[] = { 0, 255 };
+	const float *ranges[] = { range };
 
-	//Mat grayscaledMat;
-	//clahe->apply(originalMat, grayscaledMat);
+	MatND hist;
+	calcHist(&channel, 1, 0, Mat(), hist, 1, &histSize, ranges, true, false);
+
+	int hist_w = 255; int hist_h = 255;
+	int bin_w = cvRound((double)hist_w / histSize);
+
+	Mat histImage(hist_h, hist_w, CV_8UC1, Scalar(0, 0, 0));
+	normalize(hist, hist, 0, histImage.rows, NORM_MINMAX);
+
+	for (int i = 1; i < histSize; i++) {
+		line(histImage, Point(bin_w*(i - 1), hist_h - cvRound(hist.at<float>(i - 1))),
+			Point(bin_w*(i), hist_h - cvRound(hist.at<float>(i))),
+			Scalar(255, 0, 0), 2, 8, 0);
+	}
+
+	imshow("Hist", histImage);
+
+	auto model = ml::EM::create();
+	model->setClustersNumber(3);
+	model->trainEM(hist);
+
+	auto means = model->getMeans();
+	sort(means, means, CV_SORT_EVERY_COLUMN + CV_SORT_ASCENDING);
+
+	std::cout << means << std::endl;
+
+	return means.at<double>(1);
+}
+
+void processImage()
+{
+	Mat originalMat = origin.clone();
+
+	threshold(originalMat, originalMat, 30, 255, CV_THRESH_TOZERO);
+	normalize(originalMat, originalMat, 0, 255, NORM_MINMAX);
 
 	fastNlMeansDenoising(originalMat, originalMat, 3, 7, 21);
+	imshow("Original", originalMat);
 
 	Mat thresholdedMat = originalMat.clone();
 	threshold(originalMat, thresholdedMat, 30, 255, CV_THRESH_BINARY);
@@ -22,24 +63,63 @@ int main(int argc, char* argv[])
 	Mat contourOutput = thresholdedMat.clone();
 	findContours(contourOutput, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-	threshold(originalMat, originalMat, 0, 200, CV_THRESH_OTSU);
+	Rect minimalRect = boundingRect(contours[0]);
 
-	Mat contourImage = originalMat.clone();
-	Scalar colors[3];
-	colors[0] = Scalar(255, 0, 0);
+	Mat craniumMask = Mat::zeros(originalMat.size(), CV_8UC1);
 	for (size_t idx = 0; idx < contours.size(); idx++) {
-		drawContours(contourImage, contours, idx, colors[idx % 3]);
+		drawContours(craniumMask, contours, idx, Scalar(255), 0.0004f * minimalRect.size().area());
 	}
 
-	std::cout << contours.size() << "\n";
-	RotatedRect minimalRect = fitEllipse(contours[0]);
-	Point2f vertices[4];
-	minimalRect.points(vertices);
-	for (int i = 0; i < 4; i++) {
-		line(contourImage, vertices[i], vertices[(i + 1) % 4], Scalar(255), 2);
+	subtract(originalMat, craniumMask, originalMat);
+
+	Mat adaptiveThresholdedMat;
+	adaptiveThreshold(originalMat, adaptiveThresholdedMat, 
+		thresholdState.adaptiveThreshold, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,
+		thresholdState.blockSize, thresholdState.C - 100);
+	subtract(originalMat, adaptiveThresholdedMat, originalMat);
+
+	Mat structuringElement = getStructuringElement(MORPH_ELLIPSE, Size(40, 40));
+	morphologyEx(originalMat, originalMat, MORPH_CLOSE, structuringElement);
+
+	Mat resultMat = origin.clone();
+	cvtColor(resultMat, resultMat, CV_GRAY2RGB);
+	Scalar colors[3];
+	contours.clear();
+	findContours(originalMat, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	for (size_t idx = 0; idx < contours.size(); idx++) {
+		drawContours(resultMat, contours, idx, Scalar(0, 255, 0), 1);
 	}
 
-	imshow("Final", contourImage);
+	rectangle(resultMat, minimalRect, Scalar(255, 255, 255), 1, 8, 0);
+
+	imshow("Final", resultMat);
+}
+
+int main(int argc, char* argv[])
+{
+	origin = imread(argv[1], IMREAD_GRAYSCALE); //input image
+
+	thresholdState.adaptiveThreshold = getMean(origin);
+
+	namedWindow("Final", 1);
+
+	createTrackbar("Threshold", "Final", &thresholdState.adaptiveThreshold, 255, [](int, void*) {
+		processImage();
+	});
+
+	createTrackbar("Block size", "Final", &thresholdState.blockSize, 255, [](int, void*) {
+		thresholdState.blockSize += (thresholdState.blockSize + 1) % 2;
+		if (thresholdState.blockSize < 3) {
+			thresholdState.blockSize = 3;
+		}
+		processImage();
+	});
+
+	createTrackbar("Threshold constant", "Final", &thresholdState.C, 200, [](int, void*) {
+		processImage();
+	});
+
+	processImage();
 
 	waitKey();
 }
