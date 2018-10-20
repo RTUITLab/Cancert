ca539e2cc6538b49eb8db184562b08af75ff93d7
Rexagon <reide740@gmail.com>
2018-10-20 23:27:42 +0300

    Merge remote-tracking branch 'origin/maks' into develop

---
+++ WebPart/WebApplication/Services/Render/DicomRenderer.cs
@@ -0,0 +1,19 @@
+﻿using DicomParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Services.Render
+{
+    public class DicomRenderer : IRenderer
+    {
+        public Stream Render(Stream dicomStream)
+        {
+            var decoder = new DicomDecoder();
+            decoder.Init(dicomStream);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebPart/WebApplication/Services/Render/IRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Services.Render
+{
+    interface IRenderer
+    {
+        Stream Render(Stream dicomStream);
+    }
+}
